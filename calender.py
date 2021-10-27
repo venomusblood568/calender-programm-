e0303aada5aba563abdc8a2886353d8c494f9966
MR.JHA <90522738+venomusblood568@users.noreply.github.com>
2021-10-27 22:43:16 +0530

    Add files via upload

---
+++ calender.py
@@ -0,0 +1,5 @@
+import calendar    # we are importing calender 
+year = int(input("enter the year you want to see: "))  # asking for the year as input and note i am converting it in int speically
+month = int(input("enter the month you want to see: "))  # same here   i am again asking it and converting it in  int specilly 
+print(calendar.month(year,month))   # now we are printing it for output 
+
